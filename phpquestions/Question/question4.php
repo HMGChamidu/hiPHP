ef0de56f5b6a35f346c80fa5e88c1ca0292375d9
Chamidu Sandeepa <chamindusandeepa339@gmail.com>
2026-04-09 15:54:25 +0530

    Question3 4

---
+++ phpquestions/Question/question4.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Chamidu</title>
+    </head>
+
+    <body>
+        <?php 
+        $number = 25;
+     if($number>0){
+        echo "Positive";
+     }else{
+        echo "Negative";
+     } ?>
+    </body>
+</html>
